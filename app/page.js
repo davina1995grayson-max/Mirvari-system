3f7209a978a90c5e02ebfa6df29d66b6e6ec9ba5
davina1995grayson-max <davina1995grayson@gmail.com>
2026-07-11 21:13:25 +0400

    Adjust scroll offset for smooth scrolling

--- app/page.js
+++ app/page.js
@@ -292,7 +292,7 @@ const addToCart = (item, e) => {
           const el = document.getElementById(section.title);
 
 if (el) {
-  const y = el.getBoundingClientRect().top + window.pageYOffset - 170;
+  const y = el.getBoundingClientRect().top + window.pageYOffset - 180;
 
   window.scrollTo({
     top: y,

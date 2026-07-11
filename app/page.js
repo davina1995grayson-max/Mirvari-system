d601554db4518c4f20737e4906fc3ee6c2cecd98
davina1995grayson-max <davina1995grayson@gmail.com>
2026-07-11 21:14:54 +0400

    Modify scroll offset in page.js
    Adjusted scroll offset for smoother navigation.

--- app/page.js
+++ app/page.js
@@ -292,7 +292,7 @@ const addToCart = (item, e) => {
           const el = document.getElementById(section.title);
 
 if (el) {
-  const y = el.getBoundingClientRect().top + window.pageYOffset - 180;
+  const y = el.getBoundingClientRect().top + window.pageYOffset - 200;
 
   window.scrollTo({
     top: y,

95c2de982863ab65a5e976b4ba3961629886a44b
davina1995grayson-max <davina1995grayson@gmail.com>
2026-08-04 12:13:21 +0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -267,8 +267,7 @@ Zəhmət olmasa masanı seçin
     padding: "6px 12px 8px",
 
     background:
-      "linear-gradient(
-        to bottom,
+      "linear-gradient(to bottom,
         rgba(255,255,255,0.35),
         rgba(255,255,255,0.15),
         transparent

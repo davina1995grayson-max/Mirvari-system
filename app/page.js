0ee0f1c8cacd94b446c81f0d1fab1f57bb8c24e8
davina1995grayson-max <davina1995grayson@gmail.com>
2026-08-04 12:16:55 +0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -266,9 +266,6 @@ Zəhmət olmasa masanı seçin
     zIndex: 50,
     padding: "6px 12px 8px",
 
-    background:
-      "linear-gradient(to bottom, rgba(255,255,255,0.35) rgba(255,255,255,0.15), transparent )",
-
     backdropFilter: "blur(14px)",
     WebkitBackdropFilter: "blur(14px)",
 
